6cdd911865eae639d73a98386e61461166ca4555
Julien <julien.darras12@gmail.com>
2021-09-03 09:33:01 +0200

    .

--- ex5.php
+++ ex5.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,8 +8,11 @@
 </head>
 <body>
     <?php
-    $var = 0;
-    echo $var;
+    $int= (int)null;
+    echo $int;
+    $int = 3;
+    echo "<br>";
+    echo $int;
     ?>
 </body>
 </html>
